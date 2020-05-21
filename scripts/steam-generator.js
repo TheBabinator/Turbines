fcfe946942b1a014ca848a1ecbdaf0a09885cb47
TheBabinator <TheBabinator@users.noreply.github.com>
2020-05-21 17:41:17 +0100

    h

--- scripts/steam-generator.js
+++ scripts/steam-generator.js
@@ -1,15 +1,13 @@
 const steamGenerator = extendContent(PowerGenerator, "steam-generator", {
   name: "Steam Turbine",
   description: "Uses high pressure steam to spin a turbine, outputs water.",
-  tile.entity.speed: 0,
   update(tile) {
     if (tile.entity.cons.valid()) {
       tile.entity.cons.trigger();
-        tile.entity.tile.entity.speed += 0.01;
+      tile.entity.productionEfficiency += 0.01;
     } else {
-      tile.tile.entity.speed -= 0.01;
+      tile.entity.productionEfficiency -= 0.01;
     }
-    tile.entity.speed = Mathf.clamp(tile.entity.speed,0,1);
-    tile.entity.productionEfficiency = tile.entity.speed;
+    tile.entity.productionEfficiency = Mathf.clamp(tile.entity.productionEfficiency,0,1);
   }
 })
