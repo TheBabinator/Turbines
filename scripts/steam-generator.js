ff5845c508da6617d92bb05d681236c22e6e82b1
TheBabinator <TheBabinator@users.noreply.github.com>
2020-05-21 20:28:23 +0100

    numbers

--- scripts/steam-generator.js
+++ scripts/steam-generator.js
@@ -4,9 +4,9 @@ const steamGenerator = extendContent(PowerGenerator,"steam-generator",{
   update(tile) {
     if (tile.entity.cons.valid()) {
       tile.entity.cons.trigger();
-      tile.entity.productionEfficiency += tile.entity.liquids.total() / this.liquidCapacity * 0.02;
+      tile.entity.productionEfficiency += tile.entity.liquids.total() / this.liquidCapacity * 0.03;
     }
-    tile.entity.productionEfficiency -= 0.01;
+    tile.entity.productionEfficiency -= 0.02;
     tile.entity.productionEfficiency = Mathf.clamp(tile.entity.productionEfficiency,0,1);
   }
 })
