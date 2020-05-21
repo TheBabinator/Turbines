0b21be63fd34479faebf44166e77f740767972cf
TheBabinator <TheBabinator@users.noreply.github.com>
2020-05-21 21:23:36 +0100

    yessss

--- scripts/steam-generator.js
+++ scripts/steam-generator.js
@@ -2,21 +2,22 @@ const steamGenerator = extendContent(PowerGenerator,"steam-generator",{
   name: "Steam Turbine",
   description: "Uses high pressure steam to spin a turbine and generate power. Will release steam regardless of whether or not there is sufficient pressure to spin the turbine.",
   update(tile) {
+    entity = tile.ent();
     // heat is a gradual boost in production up to x3
     // this is supposed to be an incentive to use the turbine and not the stock generator
-    if (tile.entity.heat == null) tile.entity.heat = 0.0;
+    if (entity.heat == null) entity.heat = 0.0;
     // efficiency is the raw gain of power
-    if (tile.entity.efficiency == null) tile.entity.efficiency = 0.0;
+    if (entity.efficiency == null) entity.efficiency = 0.0;
 
     if (tile.entity.cons.valid()) {
       tile.entity.cons.trigger();
-      tile.entity.heat += tile.entity.liquids.total() / this.liquidCapacity * 0.00011;
-      tile.entity.efficiency += tile.entity.liquids.total() / this.liquidCapacity * 0.03;
+      entity.heat += tile.entity.liquids.total() / this.liquidCapacity * 0.00011;
+      entity.efficiency += tile.entity.liquids.total() / this.liquidCapacity * 0.03;
     }
-    tile.entity.heat -= 0.0001
-    tile.entity.heat = Mathf.clamp(tile.entity.heat,0,1)
-    tile.entity.efficiency -= 0.02;
-    tile.entity.efficiency = Mathf.clamp(tile.entity.efficiency,0,1);
-    tile.entity.productionEfficiency = tile.entity.efficiency * (1 + tile.entity.heat * 2)
+    entity.heat -= 0.0001
+    entity.heat = Mathf.clamp(entity.heat,0,1)
+    entity.efficiency -= 0.02;
+    entity.efficiency = Mathf.clamp(entity.efficiency,0,1);
+    tile.entity.productionEfficiency = entity.efficiency * (1 + entity.heat * 2)
   }
 })
