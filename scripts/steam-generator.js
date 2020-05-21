21853aff30793509bd5dfac206502421667911c6
TheBabinator <TheBabinator@users.noreply.github.com>
2020-05-21 17:20:50 +0100

    tile.entity.isGay jk dont murder me

--- scripts/steam-generator.js
+++ scripts/steam-generator.js
@@ -4,7 +4,7 @@ const steamGenerator = extendContent(PowerGenerator, "steam-generator", {
   update(tile) {
     if (tile.entity.cons.valid()) {
       tile.entity.cons.trigger();
-      tile.entity.productionEfficiency = items.get(tile.entity.cons.get(ConsumeType.item).items[0].item)/this.entity.itemCapacity;
+      tile.entity.productionEfficiency = tile.entity.items.get(tile.entity.cons.get(ConsumeType.item).items[0].item)/this.entity.itemCapacity;
     } else {
       tile.entity.productionEfficiency = 0;
     }
