168caadbb84ddcc03aa60b358055c0905f5bd439
TheBabinator <TheBabinator@users.noreply.github.com>
2020-05-21 18:34:17 +0100

    yesss

--- scripts/steam-generator.js
+++ scripts/steam-generator.js
@@ -1,12 +1,10 @@
-const steam = extendContent(Liquid,"steam",{})
-
 const steamGenerator = extendContent(PowerGenerator,"steam-generator",{
   name: "Steam Turbine",
   description: "Uses high pressure steam to spin a turbine, outputs water.",
   update(tile) {
     if (tile.entity.cons.valid()) {
       tile.entity.cons.trigger();
-      tile.entity.productionEfficiency += tile.entity.liquids.get(steam) / tile.entity.liquidCapacity * 0.02;
+      tile.entity.productionEfficiency += tile.entity.liquids.get(this.inputLiquid.liquid) / this.liquidCapacity * 0.02;
     }
     tile.entity.productionEfficiency -= 0.01;
     tile.entity.productionEfficiency = Mathf.clamp(tile.entity.productionEfficiency,0,1);
